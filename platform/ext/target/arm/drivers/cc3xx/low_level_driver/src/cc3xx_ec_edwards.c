908e76acba562b95643dfa4d1318a57462b462f6
BarschusRochus <19max60@web.de>
2025-10-08 20:38:44 +0200

    cleanup decompress, identified new todos

--- platform/ext/target/arm/drivers/cc3xx/low_level_driver/src/cc3xx_ec_edwards.c
+++ platform/ext/target/arm/drivers/cc3xx/low_level_driver/src/cc3xx_ec_edwards.c
@@ -24,6 +24,19 @@ void debug_read_and_print_reg(cc3xx_pka_reg_id_t reg, char* label){
 }
 
 
+/**
+ * @brief Decompress a compressed edwards25519 point to x,y affine coordinates.
+ *        This code is adapted from the cc312_runtime point decoding code.
+ *        It follows the steps of RFC 8032 5.1.3 and assumes that the coodrinate
+ *        is encoded as describted in 5.1.2
+ *        This whole procedure is very well explained in 
+ *        https://words.filippo.io/edwards25519-formulas/
+ * @param reg_y [in] register containing compressed y coordinate
+ * @param isOddX [in] most significant bit of compressed coordinate, 
+                    i.e. least significant bit of x coordinate
+ * @param decompressed_pt [out] result of decompression
+ * @param curve [in] curve object
+ */
 void cc3xx_lowlevel_ec_edw_decompress_point(cc3xx_pka_reg_id_t reg_y, uint32_t isOddX, 
         cc3xx_ec_point_affine *decompressed_pt, cc3xx_ec_curve_t *curve){
         // decompress: (YP) -> (XP,YP,ZP=1,TP) 
@@ -31,125 +44,71 @@ void cc3xx_lowlevel_ec_edw_decompress_point(cc3xx_pka_reg_id_t reg_y, uint32_t i
 
         uint32_t bit0;
 
-        uint32_t debug[8] = {0};
-        size_t debug_len = 32;
-        size_t debug_bytes = 8;
-
         cc3xx_pka_reg_id_t reg_x = decompressed_pt->x;
         cc3xx_pka_reg_id_t t = cc3xx_lowlevel_pka_allocate_reg();
         cc3xx_pka_reg_id_t t3 = cc3xx_lowlevel_pka_allocate_reg();
         cc3xx_pka_reg_id_t t4 = cc3xx_lowlevel_pka_allocate_reg();
         cc3xx_pka_reg_id_t t5 = cc3xx_lowlevel_pka_allocate_reg();
         
-        //edwards curve 25519 plumbing (d, sqrt_m1, q58) via curve
-        //setzt N auf 2^255-19
+        //setzt N auf 2^255-19 weil das cc3xx so vorsieht
+        //TODO: nach draussen verlagern?
         cc3xx_lowlevel_pka_set_modulus(curve->field_modulus, false, CC3XX_PKA_REG_NP);
 
-        cc3xx_lowlevel_pka_mod_mul(reg_y, reg_y, t3); //res = (r0 * r1) mod N.        //y^2 ohne reduce gibt im zweifel ein 64 byte resultat 
-        cc3xx_lowlevel_pka_read_reg(t3, debug, debug_len);
-        print_debug("t3: ",debug, debug_bytes);
-        //PKA_MOD_MUL_NFR(LEN_ID_N_BITS, EDW_REG_T3, rY, rY);                 // hwmmul(t3, y, y, n, np);  //t3 = y^2 mod n?
-        cc3xx_lowlevel_pka_mod_mul(t3, curve->param_d, t4);                                                                             
-        //PKA_MOD_MUL_NFR(LEN_ID_N_BITS, EDW_REG_T4, EDW_REG_T3, EDW_REG_D);  // hwmmul(t4, t3, ec_d, n, np); //t4 = y^2*d mod n
+        //start with creation of u and v
+        cc3xx_lowlevel_pka_mod_mul(reg_y, reg_y, t3); //y^2 
+        cc3xx_lowlevel_pka_mod_mul(t3, curve->param_d, t4); //y^2*d                                                                             
+        cc3xx_lowlevel_pka_sub_si(t3, 1, t3); //u => t3 = y^2 - 1
+        cc3xx_lowlevel_pka_add_si(t4, 1, t4); //v => t4 = d* y^2 + 1
         
-        cc3xx_lowlevel_pka_sub_si(t3, 1, t3); //u
-        //PKA_SUB_IM(LEN_ID_N_PKA_REG_BITS, EDW_REG_T3, EDW_REG_T3, 1);       // hwdec(t3, t3); //t3 = t3 - 1 = y^2 -1?
-        cc3xx_lowlevel_pka_add_si(t4, 1, t4); //v
-        //PKA_ADD_IM(LEN_ID_N_PKA_REG_BITS, EDW_REG_T4, EDW_REG_T4, 1);       // hwinc(t4, t4); //t4 = t4 + 1 = y^2*d + 1
+        //calculate x as u * v^3 * (u v^7)^((p-5)/8)
+        cc3xx_lowlevel_pka_mod_mul(t4, t4, t); //v^2
+        cc3xx_lowlevel_pka_mod_mul(t4, t, t); //t = v^3
+        cc3xx_lowlevel_pka_mod_mul(t, t, reg_x); //v^6
+        cc3xx_lowlevel_pka_mod_mul(t4, reg_x, reg_x); //v^7
+        cc3xx_lowlevel_pka_mod_mul(t3, reg_x, t5); //u*v^7
+
+        //q58 = (p-5)/8 with p = 2^255 - 19
+        cc3xx_lowlevel_pka_mod_exp(t5, curve->q58, reg_x); //(u v^7)^((p-5)/8)
+        cc3xx_lowlevel_pka_mod_mul(t3, reg_x, reg_x); //(u*v^7)^((p-5)/8) * u
+        cc3xx_lowlevel_pka_mod_mul(t, reg_x, reg_x); //(u*v^7)^((p-5)/8) * uv^3 = x
         
-        cc3xx_lowlevel_pka_mod_mul(t4, t4, t); //t=v^2
-        //PKA_MOD_MUL_NFR(LEN_ID_N_BITS, EDW_REG_T, EDW_REG_T4, EDW_REG_T4);  // hwmmul(t, t4, t4, n, np); // t = t4 * t4 = y^2*d + 1 * y^2*d + 1
-        cc3xx_lowlevel_pka_mod_mul(t4, t, t); //t=v^3
-        //PKA_MOD_MUL_NFR(LEN_ID_N_BITS, EDW_REG_T, EDW_REG_T4, EDW_REG_T);   // hwmmul(t, t4, t, n, np); // t = t4 * t = y^2*d + 1 * y^2*d + 1 * y^2*d + 1 
-        cc3xx_lowlevel_pka_mod_mul(t, t, reg_x); //x = v^6
-        //PKA_MOD_MUL_NFR(LEN_ID_N_BITS, rX, EDW_REG_T, EDW_REG_T);           // hwmmul(x, t, t, n, np); // x = t * t = y^2*d + 1 * y^2*d + 1 * y^2*d + 1 * y^2*d + 1 * y^2*d + 1 * y^2*d + 1
-        cc3xx_lowlevel_pka_mod_mul(t4, reg_x, reg_x); //x = v^6 * v = v^7
-        //PKA_MOD_MUL_NFR(LEN_ID_N_BITS, rX, rX, EDW_REG_T4);                 // hwmmul(x, x, t4, n, np); // x = x * t4 = y^2*d + 1 * y^2*d + 1 * y^2*d + 1 * y^2*d + 1 * y^2*d + 1 * y^2*d + 1 * y^2*d + 1
-        cc3xx_lowlevel_pka_mod_mul(t3, reg_x, t5); //t5 = u*v^7
-        //PKA_MOD_MUL_NFR(LEN_ID_N_BITS, EDW_REG_T5, rX, EDW_REG_T3);         // hwmmul(t5, x, t3, n, np); // t5 = x * t3 = (y^2*d + 1)^7 * (y^2-1)
-
-        //selbes spiel wie mit d -> q58 bei init vordefinieren oder hier ad hoc setzen
-        //geht ohne mod? muss ohne mod?
-        cc3xx_lowlevel_pka_mod_exp(t5, curve->q58, reg_x); // x = (u*v^7)^((p-5)/8)
-        //PKA_MOD_EXP(LEN_ID_N_BITS, rX, EDW_REG_T5, EDW_REG_Q58);            // hwmexp(x, t5, q58, n, np); // x = t5^reg_q58 // Wurzel ziehen in Magic? q58 = = (P - 5)/8 // ja, genau
-        cc3xx_lowlevel_pka_mod_mul(t3, reg_x, reg_x); //x = (u*v^7)^((p-5)/8) * u
-        //PKA_MOD_MUL_NFR(LEN_ID_N_BITS, rX, rX, EDW_REG_T3);                 // hwmmul(x, x, t3, n, np); // x = x * t3 = 
-        
-        cc3xx_lowlevel_pka_mod_mul(t, reg_x, reg_x); // x = (u*v^7)^((p-5)/8) * uv^3
-        //cc3xx_lowlevel_pka_read_reg(reg_x, debug, debug_len);
-        //print_debug("x: ",debug, debug_bytes);
-        //PKA_MOD_MUL_NFR(LEN_ID_N_BITS, rX, rX, EDW_REG_T);                  // hwmmul(x, x, t, n,np); // x = x * t
-        
-        cc3xx_lowlevel_pka_mod_mul(reg_x, reg_x, t); // t = ((u*v^7)^((p-5)/8) * uv^3)^2 = x^2
-        //cc3xx_lowlevel_pka_read_reg(t, debug, debug_len);
-        //print_debug("t = x*x: ",debug, debug_bytes);
-        //PKA_MOD_MUL_NFR(LEN_ID_N_BITS, EDW_REG_T, rX, rX);                  // hwmmul(t, x, x, n,np); // t = x * x
-        
-        //mul acc haben wir nicht, daher erst mul, dann acc um t = t4 * t + t3 zu erreichen
-        cc3xx_lowlevel_pka_mod_mul(t4, t, t); // t = t4 * t <=> t = x^2 * v
-        //cc3xx_lowlevel_pka_read_reg(t, debug, debug_len);
-        //print_debug("t = t4 + t: ",debug, debug_bytes);
-
-        cc3xx_lowlevel_pka_add(t3, t, t); // t = t + t3 <=> t = x^2 * v + u
-        //cc3xx_lowlevel_pka_read_reg(t, debug, debug_len);
-        //print_debug("t = t3 + t: ",debug, debug_bytes);
-        //PKA_MOD_MUL_ACC(LEN_ID_N_BITS, EDW_REG_T, EDW_REG_T4, EDW_REG_T, EDW_REG_T3); // hwmlap(t,t4, t, t3, n, np, 0); //t = t4 * t + t3
-
-        
-        cc3xx_lowlevel_pka_div(t, curve->field_modulus, t4, t); //t4 = t / N, t = t mod N <=> t4 = (x^2 * v + u) / N, t = (x^2 * v + u) mod N
-        printf("equal to python impl. till here\n");
-        // Divide:  Res =  OpA / OpB , OpA = OpA mod OpB - division,  #define   PKA_DIV(lenId, Res, OpA, OpB) //ahhh, also landet in opA der remainder
-        //PKA_DIV(LEN_ID_N_PKA_REG_BITS, EDW_REG_T4, EDW_REG_T, EDW_REG_N); //t4 = t / n where n is the modulus, so either 2^255-19 or the curve order...
-
-
-        //#define PKA_COMPARE_IM_STATUS(lenId, a, b,stat)
-        
-        //  CompareImmediate:  OpA ^ OpB . Rsult of compare in ZeroBitOfStatus:  If OpA == OpB then status Z = 1 
-        //#define   PKA_COMPARE_IM(lenId, OpA, OpBim)   
-        //PKA_COMPARE_IM(lenId,a,b); 
-
-        //Returns the ALU Zero-bit from PKA_STATUS register 
-        //#define PKA_GET_STATUS_ALU_OUT_ZERO(status)  
-        //PKA_GET_STATUS_ALU_OUT_ZERO(stat); 
-
-        //PKA_COMPARE_IM_STATUS(LEN_ID_N_PKA_REG_BITS, EDW_REG_T, 0 - im.val, bit0 -status); // t xor 0? 0^0 = 0;  1^1 = 0;  1^0 = 1;  0^1 = 1 --> bit0 = t xor 0 
-        //this shoul do the check for the second case of the rfc: (x^2 * v + u) = 0 mod N ? -> if so, x = x * 2^((p-1)/4) = x * sqrt(-1)
-        //I think this shall simply check whether t == 0, if so we run *sq-1, else we do not, this is case 2 of decode from RFC8032
+        //check cases for whether x is a square root
+        cc3xx_lowlevel_pka_mod_mul(reg_x, reg_x, t); // ((u*v^7)^((p-5)/8) * uv^3)^2 = x^2
+        cc3xx_lowlevel_pka_mod_mul(t4, t, t); //x^2 * v
+        cc3xx_lowlevel_pka_add(t3, t, t); //x^2 * v + u (<=> x^2 * v = -u )
+        cc3xx_lowlevel_pka_div(t, curve->field_modulus, t4, t); //(x^2 * v + u) / p = quotient (t4) + remainder (t)
+        //(x^2 * v + u) % p =?= 0     this is case 2 (x^2 * v + u <=> x^2 * v = -u )
         bit0 = cc3xx_lowlevel_pka_are_equal_si(t, 0); 
-        if(bit0) {// bit0 == 0 --> t was 0 -> (x^2 * v + u) = 0 mod N
-                printf("%ld\n", bit0);
-                printf("reg t is 0\n");
-                //sqrt_minus_one auch irgendwo hardcoden oder ad hoc einbauen
-                cc3xx_lowlevel_pka_mod_mul(reg_x, curve->sqrt_m1, reg_x); //x = x * sqrt(-1) //case 2 of rfc
-                //PKA_MOD_MUL_NFR(LEN_ID_N_BITS, rX, rX, EDW_REG_SQRTM1); // x = x * sqrt(-1)
-        }else{
-            printf("%ld\n", bit0);
-            printf("reg t is not 0\n");
-        }
-        
-        // das hier ist glaube ich nur mod fuer arme
-        // oder x = [(u*v^7)^((p-5)/8) * uv^3 *sqrt(-1)] / N
-        cc3xx_lowlevel_pka_div(reg_x, curve->field_modulus, t4, reg_x); //t4 = x / N <=> x = (u*v^7)^((p-5)/8) * uv^3 / N falls vorheriger Fall nicht hinghauen hat         
-        //PKA_DIV(LEN_ID_N_PKA_REG_BITS, EDW_REG_T4, rX, EDW_REG_N); //t4 = x / N
-
-        //das ist der letzte case aus dem decode aus 8032
-        bit0 = cc3xx_lowlevel_pka_test_bits_ui(reg_x, 0, 1); //in der hoffnung, dass das das gleiche ist
-        printf("%ld\n", bit0);
-        //PKA_READ_BIT0(LEN_ID_N_PKA_REG_BITS, rX, bit0 -bit0); //bit0 = rX[0] ? -> bit0 ist 1 -> x ist ungerade
-        if(bit0 != isOddX){ // stimmen istUngerade und sollUngerade nicht überein, dann:
-            cc3xx_lowlevel_pka_sub(curve->field_modulus, reg_x, reg_x);       
-            cc3xx_lowlevel_pka_read_reg(reg_x, debug, debug_len);
-            print_debug("x: ",debug, debug_bytes);
-            //PKA_SUB(LEN_ID_N_PKA_REG_BITS, rX, EDW_REG_N, rX); // x = N - x
+        if(bit0) {// (x^2 * v + u) % p == 0
+                //sqrt_m1 = sqrt(-1) % p
+                cc3xx_lowlevel_pka_mod_mul(reg_x, curve->sqrt_m1, reg_x); // x <-- x * 2^((p-1)/4)
+        }else{ //(x^2 * v + u) % p != 0
+            //this means case 1 is assumed - but never really checked
+            //TODO: remove empty else branch?
         }
 
-        //TODO: hier fehlt doch der case, dass x nicht berechnet werden kann, oder?
+        cc3xx_lowlevel_pka_div(reg_x, curve->field_modulus, t4, reg_x); //x / p = quotient (t4) + remainder (x) => x = x mod p
         
-        //jetzt nehmen wir an, dass x fertig berechnet ist, x und y müssen nun also in den mitgegebenen punkt
-        //TODO: msb von y auf 0 setzen
+
+
+        //last part of RFC decode procedure
+        bit0 = cc3xx_lowlevel_pka_test_bits_ui(reg_x, 0, 1); //read bit[0]
+        //printf("least significatn bit of calculated x is %ld\n", bit0);
+        
+        //TODO: if calculated_x == 0 but isOddX = 1
+        //then we're in an obvious problematic situation and decoding should fail
+
+        //decide about proper square root by checking least significant bit of x (odd/even)
+        if(bit0 != isOddX){ //if isOddx != x % 2 
+            printf("but should be %ld\ndoing x = p - x\n", isOddX);
+            cc3xx_lowlevel_pka_sub(curve->field_modulus, reg_x, reg_x); //x <-- p - x
+        }
+        
+        //TODO: write y to decompressed_pt
+        //TODO: need to care about first bit, must always be set to 0
         cc3xx_lowlevel_pka_copy(reg_y, decompressed_pt->y);
         
-        //TODO: free all the registers
+        //finished, free all registers
         cc3xx_lowlevel_pka_free_reg(t5);
         cc3xx_lowlevel_pka_free_reg(t4);
         cc3xx_lowlevel_pka_free_reg(t3);
