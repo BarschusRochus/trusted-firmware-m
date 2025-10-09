d538cf39bfb47c4b102a559088136042e678ca9b
BarschusRochus <19max60@web.de>
2025-10-09 20:49:14 +0200

    add confused application data test

--- platform/ext/target/arm/drivers/cc3xx/tests/src/cc3xx_test_ecc_edwards.c
+++ platform/ext/target/arm/drivers/cc3xx/tests/src/cc3xx_test_ecc_edwards.c
@@ -47,7 +47,7 @@ typedef struct {
     uint32_t expected_y[8];
 }cc3xx_ec_edw_point_decompress_test_data_t;
 
-cc3xx_ec_edw_point_decompress_test_data_t point_decompress_test_data =  {
+cc3xx_ec_edw_point_decompress_test_data_t point_decompress_generator =  {
     //generator point as simple test
     //when interpreted as little endian the hex number startis with 0x6 -> 0b0110 -> most significant bit is 0 -> matches lsb of expected x (0xA -> 0b10)
     .label = "Generator Point",
@@ -55,7 +55,6 @@ cc3xx_ec_edw_point_decompress_test_data_t point_decompress_test_data =  {
     .expected_odd_x = 0b00000000,
     .expected_x = {0x8F25D51A,0xC9562D60,0x9525A7B2,0x692CC760,0xFDD6DC5C,0xC0A4E231,0xCD6E53FE,0x216936D3},
     .expected_y = {0x66666658,0x66666666,0x66666666,0x66666666,0x66666666,0x66666666,0x66666666,0x66666666}
-    //0x6666666666666666666666666666666666666666666666666666666666666658
 };
 
 cc3xx_ec_edw_point_decompress_test_data_t point_decompress_one =  {
@@ -86,6 +85,20 @@ cc3xx_ec_edw_point_decompress_test_data_t point_decompress_odd_x =  {
     .expected_x = {0x322ef233, 0x91409cc0, 0x3e1be1a5, 0x5c2819f9, 0xd12da5de, 0xfcef7cf7, 0xade3587b, 0x49fda73e}, //x = 0x49fda73e ade3587b fcef7cf7 d12da5de 5c2819f9 3e1be1a5 91409cc0 322ef233
     .expected_y = {0xd676c8ed, 0x10d21f83, 0x89430b5d, 0x31282eca, 0x89924666, 0xe02c6e14, 0x98feae6f, 0x5f4825b2} //y = 0x5f4825b2 98feae6f e02c6e14 89924666 31282eca 89430b5d 10d21f83 d676c8ed
 };
+
+cc3xx_ec_edw_point_decompress_test_data_t point_decompress_r_pub_from_taler =  {
+    //uint8_t r_pub_0[32] = { 0x91, 0x7a, 0xf5, 0xcd, 0xf6, 0x4e, 0x8c, 0xba, 0x00, 0x51, 0x37, 0xf6, 0x6f, 0x14, 0xc8, 0xb5, 0xc5, 0x31, 0xa5, 0x14, 0x68, 0xc6, 0x90, 0x0c, 0xcf, 0x11, 0x7b, 0xac, 0xd8, 0x2a, 0x3e, 0x87};
+    // lil endian - 0x917af5cdf64e8cba005137f66f14c8b5c531a51468c6900ccf117bacd82a3e87  <-- input I get from rest of the application
+    // big endian - 0x873e2ad8ac7b11cf0c90c66814a531c5b5c8146ff6375100ba8c4ef6cdf57a91  <-- I reverse this one wordwise as input, 0x87 turns into 0x07 when 1 is set to 0
+    //x lil endian c757a49e 7adf3c65 178d9b45 98aadfd9 dc7a2443 133082a9 91b59d94 b5982863 - big endian: 0x632898b5, 0x949db591, 0xa9823013, 0x43247adc, 0xd9dfaa98, 0x459b8d17, 0x653cdf7a, 0x9ea457c7
+    //y lil endian 917af5cd f64e8cba 005137f6 6f14c8b5 c531a514 68c6900c cf117bac d82a3e07 - big endian: 0x073e2ad8, 0xac7b11cf, 0x0c90c668, 0x14a531c5, 0xb5c8146f, 0xf6375100, 0xba8c4ef6, 0xcdf57a91
+    .label = "taler R pub from test cases",
+    .y = {0xcdf57a91, 0xba8c4ef6, 0xf6375100, 0xb5c8146f, 0x14a531c5, 0x0c90c668, 0xac7b11cf, 0x873e2ad8}, //first bit to 1 -> 0xdf4825b298feae6fe02c6e148992466631282eca89430b5d10d21f83d676c8ed
+    .expected_odd_x = 0b00000001,
+    .expected_x = { 0x9ea457c7, 0x653cdf7a, 0x459b8d17, 0xd9dfaa98, 0x43247adc, 0xa9823013, 0x949db591, 0x632898b5},
+    .expected_y = { 0xcdf57a91, 0xba8c4ef6, 0xf6375100, 0xb5c8146f, 0x14a531c5, 0x0c90c668, 0xac7b11cf, 0x073e2ad8}
+};
+
 
 /*further decompress tests:
 - point that can't be decoded
@@ -213,13 +226,14 @@ static void ecc_edwards_tests_run(struct test_result_t *ret)
     printf("POINT DECOMPRESSION TESTS\n");
     TEST_ASSERT(cc3xx_test_ecc_edw_decompress_point(&point_decompress_odd_x) == 0, "Point decompression did not succeed");
     TEST_ASSERT(cc3xx_test_ecc_edw_decompress_point(&point_decompress_even_x) == 0, "Point decompression did not succeed");
-    TEST_ASSERT(cc3xx_test_ecc_edw_decompress_point(&point_decompress_test_data) == 0, "Point decompression did not succeed");
+    TEST_ASSERT(cc3xx_test_ecc_edw_decompress_point(&point_decompress_generator) == 0, "Point decompression did not succeed");
+    TEST_ASSERT(cc3xx_test_ecc_edw_decompress_point(&point_decompress_r_pub_from_taler) == 0, "Point decompression did not succeed");
     printf("POINT DECOMPRESSION TESTS PASSED\n\n");
 
     printf("POINT ON CURVE TESTS STARTING\n");
     TEST_ASSERT(cc3xx_test_ecc_edw_point_on_curve(&point_decompress_odd_x) == 0, "Point decompression did not succeed");
     TEST_ASSERT(cc3xx_test_ecc_edw_point_on_curve(&point_decompress_even_x) == 0, "Point decompression did not succeed");
-    TEST_ASSERT(cc3xx_test_ecc_edw_point_on_curve(&point_decompress_test_data) == 0, "Point decompression did not succeed");
+    TEST_ASSERT(cc3xx_test_ecc_edw_point_on_curve(&point_decompress_generator) == 0, "Point decompression did not succeed");
     printf("POINT ON CURVE TESTS PASSED\n\n");
 
     ret->val = TEST_PASSED;
