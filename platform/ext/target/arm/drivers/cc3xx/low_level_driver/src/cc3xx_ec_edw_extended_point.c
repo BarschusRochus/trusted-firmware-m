40a957305dc1f6b2e97882e21fb6118a735f4a0e
BarschusRochus <19max60@web.de>
2025-10-10 17:43:30 +0200

    add extended coordinates

---
+++ platform/ext/target/arm/drivers/cc3xx/low_level_driver/src/cc3xx_ec_edw_extended_point.c
@@ -0,0 +1,91 @@
+/*
+ * Copyright (c) 2024, The TrustedFirmware-M Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ */
+
+
+#include "cc3xx_ec_edw_extended_point.h"
+#include "cc3xx_error.h"
+
+#ifndef CC3XX_CONFIG_FILE
+#include "cc3xx_config.h"
+#else
+#include CC3XX_CONFIG_FILE
+#endif
+#include "cc3xx_pka.h"
+#include "cc3xx_ec.h"
+
+#include "fatal_error.h"
+
+cc3xx_ec_point_extended cc3xx_lowlevel_ec_allocate_extended_point(void)
+{
+    cc3xx_ec_point_extended res;
+
+    res.x = cc3xx_lowlevel_pka_allocate_reg();
+    res.y = cc3xx_lowlevel_pka_allocate_reg();
+    res.z = cc3xx_lowlevel_pka_allocate_reg();
+    res.t = cc3xx_lowlevel_pka_allocate_reg();
+
+    return res;
+}
+
+//free in reverse order as allocation happened
+void cc3xx_lowlevel_ec_free_extended_point(cc3xx_ec_point_extended *p){
+    cc3xx_lowlevel_pka_free_reg(p->t);
+    cc3xx_lowlevel_pka_free_reg(p->z);
+    cc3xx_lowlevel_pka_free_reg(p->y);
+    cc3xx_lowlevel_pka_free_reg(p->x);
+}
+
+//no assertions, just assume that register is set and in use
+void cc3xx_lowlevel_ec_copy_extended_point(cc3xx_ec_point_extended *p,
+                                             cc3xx_ec_point_extended *res){
+    cc3xx_lowlevel_pka_copy(p->x, res->x);
+    cc3xx_lowlevel_pka_copy(p->y, res->y);
+    cc3xx_lowlevel_pka_copy(p->z, res->z);
+    cc3xx_lowlevel_pka_copy(p->t, res->t);
+}
+
+void cc3xx_lowlevel_ec_affine_to_extended(cc3xx_ec_curve_t *curve,
+            cc3xx_ec_point_affine *p, cc3xx_ec_point_extended *res)
+{
+    //x = x
+    cc3xx_lowlevel_pka_copy(p->x, res->x);
+    //y = y
+    cc3xx_lowlevel_pka_copy(p->y, res->y);
+    
+    //z = 1 = 0 + 1
+    //that is apparently what we do when going extended
+    cc3xx_lowlevel_pka_clear(res->z);
+    cc3xx_lowlevel_pka_add_si(res->z, 0x1, res->z);
+    
+    //t = x * y
+    cc3xx_lowlevel_pka_set_modulus(curve->field_modulus, false, CC3XX_PKA_REG_NP);
+    cc3xx_lowlevel_pka_mod_mul(p->x, p->y, res->t);
+
+    cc3xx_lowlevel_pka_set_modulus(curve->order, false, CC3XX_PKA_REG_NP);
+}
+
+
+cc3xx_err_t cc3xx_lowlevel_ec_extended_to_affine(cc3xx_ec_curve_t *curve,
+                                                 cc3xx_ec_point_extended *p,
+                                                 cc3xx_ec_point_affine *res)
+{
+    cc3xx_lowlevel_pka_set_modulus(curve->field_modulus, false, CC3XX_PKA_REG_NP);
+    //(x,y) = (x / z = x * z^-1), (y/z = y *z^-1)
+    
+    //z_inv = z^-1
+    cc3xx_pka_reg_id_t z_inv = cc3xx_lowlevel_pka_allocate_reg();
+    cc3xx_lowlevel_pka_mod_inv_prime_modulus(p->z, z_inv);
+
+    //x = x * z^-1; y = y * z^-1
+    cc3xx_lowlevel_pka_mod_mul(p->x, z_inv, res->x);
+    cc3xx_lowlevel_pka_mod_mul(p->y, z_inv, res->y);
+
+    cc3xx_lowlevel_pka_set_modulus(curve->order, false, CC3XX_PKA_REG_NP);
+
+    return CC3XX_ERR_SUCCESS;
+
+}
---
+++ platform/ext/target/arm/drivers/cc3xx/low_level_driver/src/cc3xx_ec_edw_extended_point.h
@@ -0,0 +1,112 @@
+/*
+ * Copyright (c) 2024, The TrustedFirmware-M Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ */
+
+#ifndef __CC3XX_EC_EXTENDED_POINT_H__
+#define __CC3XX_EC_EXTENDED_POINT_H__
+
+#include <stdint.h>
+#include <stddef.h>
+
+#include "cc3xx_ec.h"
+
+typedef struct {
+    cc3xx_pka_reg_id_t x;
+    cc3xx_pka_reg_id_t y;
+    cc3xx_pka_reg_id_t z;
+    cc3xx_pka_reg_id_t t;
+} cc3xx_ec_point_extended;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * @brief                        Allocate a extended EC point.
+ *
+ * @return                       An initialized projective EC point.
+ */
+cc3xx_ec_point_extended cc3xx_lowlevel_ec_allocate_extended_point(void);
+
+/**
+ * @brief                        Free an extended EC point.
+ *
+ * @param[in]  p                 A pointer to the extended point to free.
+ */
+void cc3xx_lowlevel_ec_free_extended_point(cc3xx_ec_point_extended *p);
+
+/**
+ * @brief                        Copy an extended EC point.
+ *
+ * @param[in]  p                 A pointer to the extended point to copy.
+ * @param[out] res               A pointer to the extended point to copy into.
+ */
+void cc3xx_lowlevel_ec_copy_extended_point(cc3xx_ec_point_extended *p,
+                                             cc3xx_ec_point_extended *res);
+
+/**
+ * @brief                        Convert an affine point to an extended
+ *                               point. 
+                                 (x,y,z,t) = (x, y, 1, x*y)
+ *                               
+
+ * @param[in]  curve             A pointer to an initialized curve object
+ * @param[in]  p                 A pointer to the affine point to convert from.
+ * @param[out] res               A pointer to the extended point to convert to.
+ */
+void cc3xx_lowlevel_ec_affine_to_extended(cc3xx_ec_curve_t *curve,
+                                          cc3xx_ec_point_affine *p,
+                                          cc3xx_ec_point_extended *res);
+
+
+
+/**
+ * @brief                        Convert an extended point to an affine point.
+ *                               (x,y) = (x / z = x * z^-1), (y/z = y *z^-1)
+                                 And t is simply ignored.
+ *
+ * @param[in]  curve             A pointer to an initialized curve object
+ * @param[in]  p                 A pointer to the extended point to convert from.
+ * @param[out] res               A pointer to the affine point to convert to.
+ */
+cc3xx_err_t cc3xx_lowlevel_ec_extended_to_affine(cc3xx_ec_curve_t *curve,
+                                                 cc3xx_ec_point_extended *p,
+                                                 cc3xx_ec_point_affine *res);
+
+
+/**
+ * @brief                        Test if a projective point is infinity.
+ *
+ * @param[in]  p                 A pointer to the projective point to test.
+ *
+ * @return                       true if the projective point is the infinity
+ *                               point, false if it isn't.
+ */
+//bool cc3xx_lowlevel_ec_projective_point_is_infinity(cc3xx_ec_point_projective *p);
+
+/**
+ * @brief                        Convert an affine point to a Jacobian-form
+ *                               projective point, with a random Z coordinate.
+ *                               Xa = Xj / Zj^2, Ya = Yj / Zj^2.
+ *
+ * @note                         If DPA mitigations are disabled by config, this
+ *                               function falls back to having a Z coordinate of
+ *                               1.
+ *
+ * @param[in]  curve             A pointer to an initialized curve object
+ * @param[in]  p                 A pointer to the affine point to convert.
+ * @param[out] res               A pointer to the Jacobian point object.
+ */
+//void cc3xx_lowlevel_ec_affine_to_jacobian_with_random_z(cc3xx_ec_curve_t *curve,
+//                                                        cc3xx_ec_point_affine *p,
+//                                                        cc3xx_ec_point_projective *res);
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __CC3XX_EC_EXTENDED_POINT_H__ */
